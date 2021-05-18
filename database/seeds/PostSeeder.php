5dbd0d4dba5d07af6e32071d75aa029fab0cd1f1
Federica <federica.manograssi@gmail.com>
2021-05-18 23:07:26 +0200

    aggiunti dati FAKE al db

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +11,18 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=0;$i<5;$i++){
+            $newPost = new Post();
+
+            $newPost->title =$faker->sentence();
+            $newPost->author =$faker->name();
+            $newPost->title =$faker->sentence();
+            $newPost->text =$faker->sentence();
+            $newPost->category =$faker->word();
+            $newPost->publication_date =$faker->dateTimeThisDecade();
+            $newPost->save();
+        }
     }
 }
